434788327514d0100e546e6682b91c9eb646e832
Usama Habib <57696646+usamahabib545@users.noreply.github.com>
2026-05-09 01:15:25 +0500

    Update capability profile to load JSON from new path

--- lib/src/capability_profile.dart
+++ lib/src/capability_profile.dart
@@ -22,7 +22,7 @@ class CapabilityProfile {
   /// Public factory
   static Future<CapabilityProfile> load({String name = 'default'}) async {
     final content = await rootBundle
-        .loadString('lib/resources/capabilities.json');
+        .loadString('packages/esc_pos_utils/resources/capabilities.json');
     Map capabilities = json.decode(content);
 
     var profile = capabilities['profiles'][name];
